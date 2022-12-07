816ef0274f489e5297cc7f6f040e37a1a196f613
glebbash <glebbash@gmail.com>
2022-12-07 11:16:06 +0000

    feat: init vite project

---
+++ src/main.tsx
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+import ReactDOM from "react-dom/client";
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <h1>Vite + React</h1>
+      <div>
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p>Click on the Vite and React logos to learn more</p>
+    </div>
+  );
+}
